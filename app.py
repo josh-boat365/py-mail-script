20a46818b61e93e53a99b36899378385cb3c9a56
josh-boat365 <nyarko.boateng20@outlook.com>
2022-12-24 00:35:25 +0000

    improved threading to send mail with a delay 1sec for each mail sent

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 import csv
-from threading import Thread
+import time
+import threading as Thread
 from flask import Flask, render_template, request
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.app_context().push()
 app.config['SECRET'] = os.environ.get('APP_SECRET')
 
 app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER') 
@@ -27,13 +29,13 @@ def roles_types():
         'years_of_experience < 3' : [
        "Create a post webapp that:",
        "- Allows a user to create, view one, view all, update and delete a posts.",
-       "- Push to Github and add interviewer as a collaborator.",
+       "Push to Github and reply to this email with a prompt",
        "- Deploy on any platform of your choice and share link.",
        "- No need for backend or database, store data anyhow you see fit." ],
        'years_of_experience > 3 and years_of_experience < 5' : [
         "Create a post webapp that:",
        "- Allows a user to create, view one, view all, update and delete a posts.",
-       "- Push to Github and add interviewer as a collaborator.",
+       "Push to Github and reply to this email with a prompt",
        "- Deploy on any platform of your choice and share link.",
        "- No need for backend or database, store data anyhow you see fit.",
        "- UI should be responsive(should work on mobile devices). ",
@@ -41,7 +43,7 @@ def roles_types():
        'years_of_experience > 5' : [
         "Create a post webapp that:",
        "- Allows a user to create, view one, view all, update and delete a posts.",
-       "- Push to Github and add interviewer as a collaborator.",
+       "Push to Github and reply to this email with a prompt",
        "- Deploy on any platform of your choice and share link.",
        "- No need for backend or database, store data anyhow you see fit.",
        "- UI should be responsive(should work on mobile devices). ",
@@ -53,18 +55,18 @@ def roles_types():
         'years_of_experience < 3' : 
        ["Create a post API that:", 
        "- Allows a user to create, view one, view all, update and delete a posts.", 
-       "- Push to Github and add interviewer as a collaborator.", 
+       "Push to Github and reply to this email with a prompt", 
        "- Deploy on any platform of your choice and share link."], 
        'years_of_experience > 3 and years_of_experience < 5' : 
        ["Create a post API that:", 
        "- Allows a user to create, view one, view all, update and delete a posts.", 
-       "- Push to Github and add interviewer as a collaborator.", 
+       "Push to Github and reply to this email with a prompt", 
        "- Deploy on any platform of your choice and share link.",
        "- Add simple documentation on how to setup and run the app. "],
        'years_of_experience > 5' : 
        ["Create a post API that:", 
        "- Allows a user to create, view one, view all, update and delete a posts.", 
-       "- Push to Github and add interviewer as a collaborator.", 
+       "Push to Github and reply to this email with a prompt", 
        "- Deploy on any platform of your choice and share link.",
        "- Add simple documentation on how to setup and run the app. ",
        " - Dockerize solution. ",
@@ -73,19 +75,19 @@ def roles_types():
         'years_of_experience < 3' : 
          ["Create a post webapp + API:",
          "- Allows a user to create, view one, view all, update and delete a posts.",
-         "- Push to Github and add interviewer as a collaborator (two separate repos).",
+         "Push to Github and reply to this email with a prompt(two separate repos).",
          "- Deploy on any platform of your choice and share link."],
          'years_of_experience > 3 and years_of_experience < 5': 
           ["Create a post webapp + API:",
          "- Allows a user to create, view one, view all, update and delete a posts.",
-         "- Push to Github and add interviewer as a collaborator (two separate repos).",
+         "Push to Github and reply to this email with a prompt(two separate repos).",
          "- Deploy on any platform of your choice and share link.",
          "- Users should be able to authenticate and do these actions on only posts they created.",
          "- Add simple documentation on how to setup and run the app." ],
          'years_of_experience > 5' :
           ["Create a post webapp + API:",
          "- Allows a user to create, view one, view all, update and delete a posts.",
-         "- Push to Github and add interviewer as a collaborator (two separate repos).",
+         "Push to Github and reply to this email with a prompt(two separate repos).",
          "- Deploy on any platform of your choice and share link.",
          "- Users should be able to authenticate and do these actions on only posts they created.",
          "- Add simple documentation on how to setup and run the app.", 
@@ -95,7 +97,7 @@ def roles_types():
        }
 
 #function to send email
-def sendEmail(name, email, role, years_of_experience):
+def sendEmail(name, email, role, years_of_experience, delay):
     roles_types()
     #frontend messages roles
     frontend_junior = roles_types()['frontend']['years_of_experience < 3']
@@ -112,53 +114,54 @@ def sendEmail(name, email, role, years_of_experience):
     fullstack_intermediate = roles_types()['fullstack']['years_of_experience > 3 and years_of_experience < 5']
     fullstack_senior = roles_types()['fullstack']['years_of_experience > 5']
     
-    msg = Message()
-    msg.subject = "Invitation to Slightly Techie Network"
-    msg.sender = ("Slightly Techie","no-reply@gmail.com")
-    msg.recipients = [email]
-    msg.cc = ['Johndoe@gmail.com', 'Janedoe@gmail.com']
-    if role == 'frontend':
-        if years_of_experience < 3:
-            msg.html = render_template('email.html', recipient_name = name, role = 'frontend', message = frontend_junior)
-            mail.send(msg)
+    with app.app_context():
+        msg = Message()
+        msg.subject = "Invitation to Slightly Techie Network"
+        msg.sender = ("Slightly Techie","casvalabs@gmail.com")
+        msg.recipients = [email]
+        msg.cc = ['slightlytechie@gmail.com', 'wellingtoncharlottenaaodarley@gmail.com','bquansah007@gmail.com','etiboah@gmail.com','jefferykyeigenesis@gmail.com']
+        if role == 'frontend':
+            if years_of_experience < 3:
+                msg.html = render_template('email.html', recipient_name = name, role = 'frontend', message = frontend_junior)
+                mail.send(msg)
+
+            elif years_of_experience in range(3,6):
+                msg.html = render_template('email.html', recipient_name = name, role = 'frontend', message = frontend_intermediate)
+                mail.send(msg)
+                
+            elif years_of_experience > 5:
+                msg.html = render_template('email.html', recipient_name = name, role = 'frontend', message = frontend_senior)
+                mail.send(msg)
+                
+        if role == 'backend':
+            if years_of_experience < 3:
+                msg.html = render_template('email.html', recipient_name = name, role = 'backend', message = backend_junior)
+                mail.send(msg)
+                
+            elif years_of_experience in range(3,6):
+                msg.html = render_template('email.html', recipient_name = name, role = 'backend', message = backend_intermediate)
+                mail.send(msg)
+                
+            elif years_of_experience > 5:
+                msg.html = render_template('email.html', recipient_name = name, role = 'backend', message = backend_senior)
+                mail.send(msg)
             
-        elif years_of_experience in range(3,6):
-            msg.html = render_template('email.html', recipient_name = name, role = 'frontend', message = frontend_intermediate)
-            mail.send(msg)
-            
-        elif years_of_experience > 5:
-            msg.html = render_template('email.html', recipient_name = name, role = 'frontend', message = frontend_senior)
-            mail.send(msg)
-            
-    if role == 'backend':
-        if years_of_experience < 3:
-            msg.html = render_template('email.html', recipient_name = name, role = 'backend', message = backend_junior)
-            mail.send(msg)
-            
-        elif years_of_experience in range(3,6):
-            msg.html = render_template('email.html', recipient_name = name, role = 'backend', message = backend_intermediate)
-            mail.send(msg)
-            
-        elif years_of_experience > 5:
-            msg.html = render_template('email.html', recipient_name = name, role = 'backend', message = backend_senior)
-            mail.send(msg)
-        
-    if role == 'fullstack':
-        if years_of_experience < 3:
-            msg.html = render_template('email.html', recipient_name = name, role = 'fullstack', message = fullstack_junior)
-            mail.send(msg)
-            
-        elif years_of_experience in range(3,6):
-            msg.html = render_template('email.html', recipient_name = name, role = 'fullstack', message = fullstack_intermediate)
-            mail.send(msg)
-            
-        elif years_of_experience > 5:
-            msg.html = render_template('email.html', recipient_name = name, role = 'fullstack', message = fullstack_senior)
-            mail.send(msg)
-            
-        else:
-            msg.html = render_template('email.html', recipient_name = name, message = "Sorry.., we are experiencing some technical difficulties. we will get back to you shortly.")
-            mail.send(msg)  
+        if role == 'fullstack':
+            if years_of_experience < 3:
+                msg.html = render_template('email.html', recipient_name = name, role = 'fullstack', message = fullstack_junior)
+                mail.send(msg)
+                
+            elif years_of_experience in range(3,6):
+                msg.html = render_template('email.html', recipient_name = name, role = 'fullstack', message = fullstack_intermediate)
+                mail.send(msg)
+                
+            elif years_of_experience > 5:
+                msg.html = render_template('email.html', recipient_name = name, role = 'fullstack', message = fullstack_senior)
+                mail.send(msg)
+                
+            else:
+                msg.html = render_template('email.html', recipient_name = name, message = "Sorry.., we are experiencing some technical difficulties. we will get back to you shortly.")
+                mail.send(msg)  
 
 # function to get name,email,role and years_of_experience from csv file
 def read_csv(filename):
@@ -166,7 +169,7 @@ def read_csv(filename):
   with open(filename, "r") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-      rows.append((row['ï»¿Name'],
+      rows.append((row['Name'],
       row['Email'],
       row['Role'],
       row['Years_of_experience'] ))
@@ -175,16 +178,29 @@ def read_csv(filename):
 
 #function to send email to multiple persons at the same time on a different thread        
 def send_emails_to_respective_persons():
-    name_email_pairs = read_csv('emails_csv.csv') #[('Joshua Nyarko Boateng', 'test1@gmail.com', 'backend', '8'), ('Kwame Kay', 'test2@gmail.com', 'fullstack', '1'), ('krypton', 'test3@gmail.com', 'frontend', '12')]
-
+    #name_email_pairs = read_csv('new_emails.csv') #[('Joshua Nyarko Boateng', 'test1@gmail.com', 'backend', '8'), ('Kwame Kay', 'test2@gmail.com', 'fullstack', '1'), ('krypton', 'test3@gmail.com', 'frontend', '12')]
+    name_email_pairs = read_csv('interviewees1.csv')
+                        
+    delay = 1
+    num_of_emails_sent = 0
+    Threads = []
     # a loop to get the name and email of each person from the list 'name_email_pairs'
     for name, email, role, years_of_experience in name_email_pairs:
         # name, email, role, years_of_experience are passed to the thread, making use of the sendEmail function, so that the thread can send the email to respective persons
-        thread = Thread(target=sendEmail(name=name,email=email, role=role.lower(), years_of_experience=int(years_of_experience)))
+        thread = Thread.Timer(delay, sendEmail, args=[name,email, role.lower(),int(years_of_experience), delay])
         # the thread is started
         thread.start()
-        # thread.join()
+        Threads.append(thread)
+
+        num_of_emails_sent += 1
+        delay += 1
+    print(f"Sent {num_of_emails_sent} emails in {delay} seconds")
+
+    for threadd in Threads:
+        threadd.join()
+    print(f"Number of Threads = {len(Threads)}")
         
+
 
 
 @app.route('/', methods=['GET','POST'])
@@ -193,7 +209,6 @@ def home():
         send_emails_to_respective_persons()
         
         return "Mail Sent!!"
-        
        
     return render_template('index.html')
 
