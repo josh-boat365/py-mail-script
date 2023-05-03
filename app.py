a4a3d9c447c9c27dc0c62df0132ff364c133b5e4
josh-boat365 <nyarko.boateng20@outlook.com>
2023-05-03 23:38:20 +0000

    modified function for sending email for better efficiency

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import csv
 import time
-import threading as Thread
+import threading 
 from flask import Flask, render_template, request
 from flask_mail import Mail, Message
 
@@ -84,72 +84,43 @@ def roles_types():
     }
     return roles
 
-#function to send email
+# define email messages for each role and experience level
+email_messages = {
+    'frontend': {
+        '<3': roles_types()['frontend']['requirements'] + roles_types()['frontend']['experience_levels']['years_of_experience < 3'],
+        '3-5': roles_types()['frontend']['requirements'] + roles_types()['frontend']['experience_levels']['years_of_experience < 5'],
+        '>5': roles_types()['frontend']['requirements'] + roles_types()['frontend']['experience_levels']['years_of_experience >= 5']
+    },
+    'backend': {
+        '<3': roles_types()['backend']['requirements'] + roles_types()['backend']['experience_levels']['years_of_experience < 3'],
+        '3-5': roles_types()['backend']['requirements'] + roles_types()['backend']['experience_levels']['years_of_experience < 5'],
+        '>5': roles_types()['backend']['requirements'] + roles_types()['backend']['experience_levels']['years_of_experience >= 5']
+    },
+    'fullstack': {
+        '<3': roles_types()['fullstack']['requirements'] + roles_types()['fullstack']['experience_levels']['years_of_experience < 3'],
+        '3-5': roles_types()['fullstack']['requirements'] + roles_types()['fullstack']['experience_levels']['years_of_experience < 5'],
+        '>5': roles_types()['fullstack']['requirements'] + roles_types()['fullstack']['experience_levels']['years_of_experience >= 5']
+    }
+}
+
+# function to send email
 def sendEmail(name, email, role, years_of_experience, delay):
-    roles_types()
-    #frontend messages roles
-    frontend_junior = roles_types()['frontend']['requirements'] + roles_types()['frontend']['experience_levels']['years_of_experience < 3']
-    frontend_intermediate = roles_types()['frontend']['requirements'] + roles_types()['frontend']['experience_levels']['years_of_experience > 3 and years_of_experience < 5']
-    frontend_senior = roles_types()['frontend']['requirements'] + roles_types()['frontend']['experience_levels']['years_of_experience > 5']
-
-    #backend messages roles
-    backend_junior = roles_types()['backend']['requirements'] + roles_types()['backend']['years_of_experience < 3']
-    backend_intermediate = roles_types()['backend']['requirements'] + roles_types()['backend']['experience_levels']['years_of_experience > 3 and years_of_experience < 5']
-    backend_senior = roles_types()['backend']['requirements'] + roles_types()['backend']['experience_levels']['years_of_experience > 5']
-
-    #fullstack messages roles
-    fullstack_junior = roles_types()['fullstack']['requirements'] + roles_types()['fullstack']['experience_levels']['years_of_experience < 3']
-    fullstack_intermediate = roles_types()['fullstack']['requirements'] + roles_types()['fullstack']['experience_levels']['years_of_experience > 3 and years_of_experience < 5']
-    fullstack_senior = roles_types()['fullstack']['requirements'] + roles_types()['fullstack']['experience_levels']['years_of_experience > 5']
-     
     with app.app_context():
         msg = Message()
         msg.subject = "Invitation to Slightly Techie Network"
         msg.sender = ("Slightly Techie", "casvalabs@gmail.com" )
         msg.recipients = [email]
-        msg.cc =  app.config['COPY_TO_MAILS']
-        if role == 'frontend':
+        msg.cc =  ['slightlytechie@gmail.com', 'wellingtoncharlottenaaodarley@gmail.com','bquansah007@gmail.com','etiboah@gmail.com','kwame.nyarko365@gmail.com']
+        if role in email_messages:
             if years_of_experience < 3:
-                msg.html = render_template('email.html', recipient_name = name, role = 'frontend', message = frontend_junior)
-                mail.send(msg)
-
-            elif years_of_experience in range(3,6):
-                msg.html = render_template('email.html', recipient_name = name, role = 'frontend', message = frontend_intermediate)
-                mail.send(msg)
-                
+                msg.html = render_template('email.html', recipient_name=name, role=role, message=email_messages[role]['<3'])
+            elif years_of_experience in range(3, 6):
+                msg.html = render_template('email.html', recipient_name=name, role=role, message=email_messages[role]['3-5'])
             elif years_of_experience > 5:
-                msg.html = render_template('email.html', recipient_name = name, role = 'frontend', message = frontend_senior)
-                mail.send(msg)
-                
-        if role == 'backend':
-            if years_of_experience < 3:
-                msg.html = render_template('email.html', recipient_name = name, role = 'backend', message = backend_junior)
-                mail.send(msg)
-                
-            elif years_of_experience in range(3,6):
-                msg.html = render_template('email.html', recipient_name = name, role = 'backend', message = backend_intermediate)
-                mail.send(msg)
-                
-            elif years_of_experience > 5:
-                msg.html = render_template('email.html', recipient_name = name, role = 'backend', message = backend_senior)
-                mail.send(msg)
-            
-        if role == 'fullstack':
-            if years_of_experience < 3:
-                msg.html = render_template('email.html', recipient_name = name, role = 'fullstack', message = fullstack_junior)
-                mail.send(msg)
-                
-            elif years_of_experience in range(3,6):
-                msg.html = render_template('email.html', recipient_name = name, role = 'fullstack', message = fullstack_intermediate)
-                mail.send(msg)
-                
-            elif years_of_experience > 5:
-                msg.html = render_template('email.html', recipient_name = name, role = 'fullstack', message = fullstack_senior)
-                mail.send(msg)
-                
+                msg.html = render_template('email.html', recipient_name=name, role=role, message=email_messages[role]['>5'])
             else:
-                msg.html = render_template('email.html', recipient_name = name, message = "Sorry.., we are experiencing some technical difficulties. we will get back to you shortly.")
-                mail.send(msg)  
+                msg.html = render_template('email.html', recipient_name=name, message="Sorry.., we are experiencing some technical difficulties. we will get back to you shortly.")
+            mail.send(msg)
 
 # send reminder mail 
 def sendEMailReminder(name, email):
@@ -169,46 +140,49 @@ def sendEMailReminder(name, email):
     mail.send(msg)
     
 
-# function to get name,email,role and years_of_experience from csv file
+
+# function to get name, email, role and years_of_experience from csv file
 def read_csv(filename):
-  rows = []
-  with open(filename, "r") as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-      rows.append((row['Name'],
-      row['Email'],
-      row['Role'],
-      row['Years_of_experience'] ))
+    rows = []
+    try:
+        with open(filename, "r") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                rows.append((row['Name'], row['Email'], row['Role'], int(row['Years_of_experience'])))
+    except FileNotFoundError:
+        print(f"File not found: {filename}")
+    except ValueError:
+        print(f"Invalid value in CSV file: {filename}")
+
     return rows
 
 
 #function to send email to multiple persons at the same time on a different thread  
-      
 def send_emails_to_respective_persons():
-    name_email_pairs = [('Jerry Wilson', 'wilsonjerry182@gmail.com', 'backend', '1')] #read_csv('emails.csv') #[('Joshua Nyarko Boateng', 'test1@gmail.com', 'backend', '8'), ('Kwame Kay', 'test2@gmail.com', 'fullstack', '1'), ('krypton', 'test3@gmail.com', 'frontend', '12')]
-                        
+    name_email_pairs = read_csv('emails.csv')  #[('Joshua Nyarko Boateng', 'test1@gmail.com', 'backend', '8'), ('Kwame Kay', 'test2@gmail.com', 'fullstack', '1'), ('krypton', 'test3@gmail.com', 'frontend', '12')]
     delay = 0
     num_of_emails_sent = 0
-    Threads = []
-    # a loop to get the name and email of each person from the list 'name_email_pairs'
-    for name, email, role, years_of_experience in name_email_pairs:
-        # name, email, role, years_of_experience are passed to the thread, making use of the sendEmail function, so that the thread can send the email to respective persons
-        # years = int(years_of_experience)
-        # r = role.lower()
-        # sendEmail(name,email,r,years,time.sleep(delay))
-        thread = Thread.Timer(delay, sendEmail, args=[name,email, role.lower(),int(years_of_experience), delay])
-        # # # the thread is started
-        thread.start()
-        Threads.append(thread)
+    threads = []
 
+    # def send_email_wrapper(name, email, role, years_of_experience, delay):
+    #     time.sleep(delay)
+    #     sendEmail(name, email, role.lower(), int(years_of_experience), delay)
+
+        
+
+    for name, email, role, years_of_experience in name_email_pairs:
+        thread = threading.Thread(target=sendEmail, args=(name, email, role.lower(), int(years_of_experience), time.sleep(delay)))
+        threads.append(thread)
+        thread.start()
         num_of_emails_sent += 1
         delay += 1
         print(f"Sent {num_of_emails_sent} emails in {delay} seconds")
 
-    for threadd in Threads:
-        threadd.join()
-    print(f"Number of Threads = {len(Threads)}")
-        
+    for thread in threads:
+        thread.join()
+    
+    print(f"Number of Threads = {len(threads)}")
+
 def send_reminder_email():
     name_email_pairs = read_csv('reminder_emails.csv')
     reminderMailsSent = 0
@@ -228,8 +202,10 @@ def home():
             #send_reminder_email()
         except ValueError as e:
             print('Error: ', e)
+            return f"ValueError:,{e} "
         except Exception as exp:
             print('An Error Occured!!: ', exp)
+            return f" An Error Occured !!:,{exp} "
         finally:
             return "Mail Sent!!"
         
