176a6f240b0a940dda408e12abe030d03cf309b9
josh-boat365 <nyarko.boateng20@outlook.com>
2023-05-03 22:18:01 +0000

    modified function for role types, added exceptions,

--- app.py
+++ app.py
@@ -21,95 +21,87 @@ mail = Mail(app)
 
 
 def roles_types():
-    return   {'frontend':{
-        'years_of_experience < 3' : [
-       "Create a post webapp that:",
-       "- Allows a user to create, view one, view all, update and delete a posts.",
-       "Push to Github and reply to this email with a prompt",
-       "- Deploy on any platform of your choice and share link.",
-       "- No need for backend or database, store data anyhow you see fit." ],
-       'years_of_experience > 3 and years_of_experience < 5' : [
-        "Create a post webapp that:",
-       "- Allows a user to create, view one, view all, update and delete a posts.",
-       "Push to Github and reply to this email with a prompt",
-       "- Deploy on any platform of your choice and share link.",
-       "- No need for backend or database, store data anyhow you see fit.",
-       "- UI should be responsive(should work on mobile devices). ",
-       "- Add simple documentation on how to setup and run the app." ],
-       'years_of_experience > 5' : [
-        "Create a post webapp that:",
-       "- Allows a user to create, view one, view all, update and delete a posts.",
-       "Push to Github and reply to this email with a prompt",
-       "- Deploy on any platform of your choice and share link.",
-       "- No need for backend or database, store data anyhow you see fit.",
-       "- UI should be responsive(should work on mobile devices). ",
-       "- Add simple documentation on how to setup and run the app.",
-       "- UX should be taken into consideration.",
-       "- Setup unit test to cover at least 50% of the code. " ] 
-       },
-       'backend':{
-        'years_of_experience < 3' : 
-       ["Create a post API that:", 
-       "- Allows a user to create, view one, view all, update and delete a posts.", 
-       "Push to Github and reply to this email with a prompt", 
-       "- Deploy on any platform of your choice and share link."], 
-       'years_of_experience > 3 and years_of_experience < 5' : 
-       ["Create a post API that:", 
-       "- Allows a user to create, view one, view all, update and delete a posts.", 
-       "Push to Github and reply to this email with a prompt", 
-       "- Deploy on any platform of your choice and share link.",
-       "- Add simple documentation on how to setup and run the app. "],
-       'years_of_experience > 5' : 
-       ["Create a post API that:", 
-       "- Allows a user to create, view one, view all, update and delete a posts.", 
-       "Push to Github and reply to this email with a prompt", 
-       "- Deploy on any platform of your choice and share link.",
-       "- Add simple documentation on how to setup and run the app. ",
-       " - Dockerize solution. ",
-       "- Setup unit test to cover at least 50% of the code. "]},
-         'fullstack':{
-        'years_of_experience < 3' : 
-         ["Create a post webapp + API:",
-         "- Allows a user to create, view one, view all, update and delete a posts.",
-         "Push to Github and reply to this email with a prompt(two separate repos).",
-         "- Deploy on any platform of your choice and share link."],
-         'years_of_experience > 3 and years_of_experience < 5': 
-          ["Create a post webapp + API:",
-         "- Allows a user to create, view one, view all, update and delete a posts.",
-         "Push to Github and reply to this email with a prompt(two separate repos).",
-         "- Deploy on any platform of your choice and share link.",
-         "- Users should be able to authenticate and do these actions on only posts they created.",
-         "- Add simple documentation on how to setup and run the app." ],
-         'years_of_experience > 5' :
-          ["Create a post webapp + API:",
-         "- Allows a user to create, view one, view all, update and delete a posts.",
-         "Push to Github and reply to this email with a prompt(two separate repos).",
-         "- Deploy on any platform of your choice and share link.",
-         "- Users should be able to authenticate and do these actions on only posts they created.",
-         "- Add simple documentation on how to setup and run the app.", 
-         "- Dockerize solution both webpp and api.", 
-         "- Setup unit test to cover at least 50% of the code."]
-         }
-       }
+    roles = {
+        'frontend': {
+            'requirements': [
+                "Create a post webapp that:",
+                "- Allows a user to create, view one, view all, update and delete a posts.",
+                "Push to Github and reply to this email with a prompt",
+                "- Deploy on any platform of your choice and share link.",
+                "- No need for backend or database, store data anyhow you see fit."
+            ],
+            'experience_levels': {
+                'years_of_experience < 3': [],
+                'years_of_experience < 5': [
+                    "- UI should be responsive (should work on mobile devices). ",
+                    "- Add simple documentation on how to setup and run the app."
+                ],
+                'years_of_experience >= 5': [
+                    "- UX should be taken into consideration.",
+                    "- Setup unit test to cover at least 50% of the code."
+                ]
+            }
+        },
+        'backend': {
+            'requirements': [
+                "Create a post API that:", 
+                "- Allows a user to create, view one, view all, update and delete a posts.", 
+                "Push to Github and reply to this email with a prompt", 
+                "- Deploy on any platform of your choice and share link."
+            ],
+            'experience_levels': {
+                'years_of_experience < 3': [],
+                'years_of_experience < 5': [
+                    "- Add simple documentation on how to setup and run the app."
+                ],
+                'years_of_experience >= 5': [
+                    "- Dockerize solution.",
+                    "- Setup unit test to cover at least 50% of the code."
+                ]
+            }
+        },
+        'fullstack': {
+            'requirements': [
+                "Create a post webapp + API:",
+                "- Allows a user to create, view one, view all, update and delete a posts.",
+                "Push to Github and reply to this email with a prompt (two separate repos).",
+                "- Deploy on any platform of your choice and share link."
+            ],
+            'experience_levels': {
+                'years_of_experience < 3': [],
+                'years_of_experience < 5': [
+                    "- Users should be able to authenticate and do these actions on only posts they created.",
+                    "- Add simple documentation on how to setup and run the app."
+                ],
+                'years_of_experience >= 5': [
+                    "- Users should be able to authenticate and do these actions on only posts they created.",
+                    "- Add simple documentation on how to setup and run the app.",
+                    "- Dockerize solution both webapp and API.",
+                    "- Setup unit test to cover at least 50% of the code."
+                ]
+            }
+        }
+    }
+    return roles
 
 #function to send email
 def sendEmail(name, email, role, years_of_experience, delay):
     roles_types()
     #frontend messages roles
-    frontend_junior = roles_types()['frontend']['years_of_experience < 3']
-    frontend_intermediate = roles_types()['frontend']['years_of_experience > 3 and years_of_experience < 5']
-    frontend_senior = roles_types()['frontend']['years_of_experience > 5']
+    frontend_junior = roles_types()['frontend']['requirements'] + roles_types()['frontend']['experience_levels']['years_of_experience < 3']
+    frontend_intermediate = roles_types()['frontend']['requirements'] + roles_types()['frontend']['experience_levels']['years_of_experience > 3 and years_of_experience < 5']
+    frontend_senior = roles_types()['frontend']['requirements'] + roles_types()['frontend']['experience_levels']['years_of_experience > 5']
 
     #backend messages roles
-    backend_junior = roles_types()['backend']['years_of_experience < 3']
-    backend_intermediate = roles_types()['backend']['years_of_experience > 3 and years_of_experience < 5']
-    backend_senior = roles_types()['backend']['years_of_experience > 5']
+    backend_junior = roles_types()['backend']['requirements'] + roles_types()['backend']['years_of_experience < 3']
+    backend_intermediate = roles_types()['backend']['requirements'] + roles_types()['backend']['experience_levels']['years_of_experience > 3 and years_of_experience < 5']
+    backend_senior = roles_types()['backend']['requirements'] + roles_types()['backend']['experience_levels']['years_of_experience > 5']
 
     #fullstack messages roles
-    fullstack_junior = roles_types()['fullstack']['years_of_experience < 3']
-    fullstack_intermediate = roles_types()['fullstack']['years_of_experience > 3 and years_of_experience < 5']
-    fullstack_senior = roles_types()['fullstack']['years_of_experience > 5']
-    
+    fullstack_junior = roles_types()['fullstack']['requirements'] + roles_types()['fullstack']['experience_levels']['years_of_experience < 3']
+    fullstack_intermediate = roles_types()['fullstack']['requirements'] + roles_types()['fullstack']['experience_levels']['years_of_experience > 3 and years_of_experience < 5']
+    fullstack_senior = roles_types()['fullstack']['requirements'] + roles_types()['fullstack']['experience_levels']['years_of_experience > 5']
+     
     with app.app_context():
         msg = Message()
         msg.subject = "Invitation to Slightly Techie Network"
@@ -193,7 +185,7 @@ def read_csv(filename):
 #function to send email to multiple persons at the same time on a different thread  
       
 def send_emails_to_respective_persons():
-    name_email_pairs = read_csv('emails.csv') #[('Joshua Nyarko Boateng', 'test1@gmail.com', 'backend', '8'), ('Kwame Kay', 'test2@gmail.com', 'fullstack', '1'), ('krypton', 'test3@gmail.com', 'frontend', '12')]
+    name_email_pairs = [('Jerry Wilson', 'wilsonjerry182@gmail.com', 'backend', '1')] #read_csv('emails.csv') #[('Joshua Nyarko Boateng', 'test1@gmail.com', 'backend', '8'), ('Kwame Kay', 'test2@gmail.com', 'fullstack', '1'), ('krypton', 'test3@gmail.com', 'frontend', '12')]
                         
     delay = 0
     num_of_emails_sent = 0
@@ -230,10 +222,19 @@ def send_reminder_email():
 @app.route('/', methods=['GET','POST'])
 def home():
     if request.method == 'POST':
-        send_emails_to_respective_persons()
-        #send_reminder_email()
+
+        try:
+            send_emails_to_respective_persons()
+            #send_reminder_email()
+        except ValueError as e:
+            print('Error: ', e)
+        except Exception as exp:
+            print('An Error Occured!!: ', exp)
+        finally:
+            return "Mail Sent!!"
         
-        return "Mail Sent!!"
+
+        
        
     return render_template('index.html')
 
